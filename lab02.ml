b89da7c3bd99e62bed6b76414d466490161a407b
Mubarak Muse <musex025@umn.edu>
2017-09-15 15:33:16 -0500

    fixig errors

--- lab02.ml
+++ lab02.ml
@@ -1,24 +1,24 @@
 (* OCaml file for lab 02.
    Fix the errors in this file. *)
 
-let five = (2 + )
+let five = (2 + 3 )
 
-let fun x = x.[2]
+let myfun x = x.[2]
 
-let mult x = x * y
-
-let plus_two x = x + two
+let mult x y = x * y
 let two = 2
 
-let and3 a b c = x && y && z
+let plus_two x = x + two
 
-let helloworld = "hello" + "world"
+let and3 a b c = a && b && c
+
+let helloworld = "hello" ^  "world"
 
 let three_or_ten x = if x then 3 else 10
-let three = three_or_ten 1
-let ten = three_or_ten 5 < 2
+let three = three_or_ten true
+let ten = three_or_ten (5 < 2)
 
 
 let nine = mult (three_or_ten true) (three_or_ten (2 < 4))
 
-let mult_or_plus a b = if (three_or_ten x < 5) then (2 * ) else (2 + b)
+let mult_or_plus a b = if (three_or_ten a < 5) then (1 *2 ) else (2 + 2)
