635406035eee2889ea0ca941edafe324f71be2de
mubarak <musex025@csel-kh4240-03.cselabs.umn.edu>
2017-09-22 13:43:46 -0500

    hw

--- hw1.ml
+++ hw1.ml
@@ -2,6 +2,22 @@ let rec gcd a b =
 	if a = b then a
  	else
   		if a > b then gcd (a-b) b
-  		else gcd a (b-a) ;; 
+  		else gcd a (b-a) ;;
 
+let reduced_form tuple1 =
+  match tuple1 with
+  |(_,_) ->  0
+  |(x,y) -> if gcd x y = 1
+    then 0
+    else (x,y) ;;
 
+let rec fromMtoN m n =
+  if m>n
+    then []
+  else m:: fromMtoN (m+1) n;;
+
+let rec everyEven list1 =
+  match list1 with
+  |[] -> []
+  |[h::l::t] -> l::everyEven t
+  |[h::t]-> t ;;
