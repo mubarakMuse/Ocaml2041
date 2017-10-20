4a9e967bc0b874b94128e4f759156acf57c8428b
Mubarak Muse <musex025@umn.edu>
2017-10-20 16:58:27 -0500

    add

--- lab07.ml
+++ lab07.ml
@@ -15,3 +15,34 @@ let rec reduce f lst u =
   | [] -> u
   | (h::t) -> f h (reduce f t u)
     
+let rec appears a list1 = 
+  match list1 with
+  | [] -> false
+  | h::t-> if h=a then true else appears a t
+
+let union l1 l2 = accumulate (fun x y -> if (appears x y) then y else x::y) l1 l2
+
+let  unzip l = reduce (fun (x,y) (l1,l2)-> (x::l1,y::l2) ) l ([],[])
+
+let zip l1 l2 = map2 (fun x y -> (x,y))  l1 l2
+
+
+
+(*
+Compose: 
+1- 'a->'b->'c (before = )
+2- 'a->'b->'d->'c (from fun  x)
+3- 'a->('d->'e)->'d->'c (form (g x))
+4- ('e -> f')->('d->'e)->'d->'f (From (f (g x)))
+5- ('a->'b)-> ('c->'a) -> 'c -> 'b (rewirte it )
+
+compose (fun x -> x+x) (fun x -> x + " 7") 10 //  doesnt work
+compose (fun x -> x+x) (fun x -> x + 7) 10 // works
+
+forsome: 
+1- 'a -> 'b -> 'c
+2- 'a -> 'b list -> bool
+3- ('b -> bool) -> 'b list -> bool
+
+
+*)
